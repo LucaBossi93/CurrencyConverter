c7c5c793e4e46a24ab7a95d8747188f119d15623
Luca <bossi.luca@hotmail.it>
2020-02-19 21:46:54 +0100

    added comments and tests

--- venv/converter.py
+++ venv/converter.py
@@ -19,32 +19,39 @@ response = requests.request("GET", url, headers=headers, data=payload)
 
 response_encoded = response.text.encode('utf8')
 
+# Converts the xml file obtained from the website into a dictionary
 doc = xmltodict.parse(response_encoded)
 
 with open('data.json', 'w') as fp:
     json.dump(doc, fp, sort_keys=True, indent=4)
-lista = doc["gesmes:Envelope"]["Cube"]["Cube"]
+
+# From the source dictionary, another dictionary is created containing only the data useful for the application.
+cur_list = doc["gesmes:Envelope"]["Cube"]["Cube"]
+
 new_dict = {}
 
-for day in lista:
+for day in cur_list:
     new_dict[day["@time"]] = {}
     new_dict[day["@time"]]["EUR"] = float(1)
     for row in day["Cube"]:
         new_dict[day["@time"]][row["@currency"]] = float(row["@rate"])
 
+# a dataframe is created using pandas
 dfcur = pd.DataFrame.from_dict(new_dict)
-
-datetime_object = datetime.strptime('2014-02-20', '%Y-%m-%d')
-print(datetime_object)
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def homes():
+    return "This Application allows you to convert currencies."
 
 
 @app.route("/converter", methods=['GET'])
 def get_input():
+    """
+    Function that receives data via GET, validates it and performs currency conversion.
+
+    :return: the converted currency is returned as json
+    """
     amount = (request.args.get("amount"))
     src_currency = (request.args.get("src_currency"))
     dest_currency = request.args.get("dest_currency")
@@ -61,19 +68,35 @@ def get_input():
         return str(e)
 
     converted_value = _convert_currency(amount, src_currency, dest_currency, reference_date)
-    return '''<h1>The language value is: {}</h1>'''.format(converted_value)
+    return jsonify(amount=converted_value, currency=dest_currency)
 
 
 def _validate_inputs(amount, src_currency, dest_currency, reference_date):
+    """
+    Function that recalls the various input validation methods
+
+    :param amount: Amount of currency you want to convert
+    :param src_currency: Source currency code received as input.
+    :param dest_currency: Destination currency code
+    :param reference_date: Date on which the operation is executed
+    :return: Return validated inputs
+    """
     return _validate_amount(amount), _validate_src_currency(src_currency), _validate_dest_currency(
         dest_currency), _validate_reference_date(reference_date)
 
 
 def _validate_src_currency(src_currency):
+    """
+    Function that checks that the source currency code as input is correct.
+    If the value is invalid it generates an error, otherwise it returns the value it received as converted input.
+
+    :param src_currency: Source currency code received as input.
+    :return: Validated source currency code
+    """
     try:
         src_currency = src_currency.upper()
     except AttributeError:
-        raise AttributeError("")
+        raise AttributeError("src_currency code is not valid") from AttributeError
     try:
         if dfcur.loc[src_currency].any():
             return src_currency
@@ -82,10 +105,17 @@ def _validate_src_currency(src_currency):
 
 
 def _validate_dest_currency(dest_currency):
+    """
+    Function that checks that the destination currency code as input is correct.
+    If the value is invalid it generates an error, otherwise it returns the value it received as converted input.
+
+    :param dest_currency: Destination currency code received as input.
+    :return: Validated destination currency code
+    """
     try:
         dest_currency = dest_currency.upper()
     except AttributeError:
-        raise
+        raise AttributeError("src_currency code is not valid") from AttributeError
     try:
         if dfcur.loc[dest_currency].any():
             return dest_currency
@@ -94,6 +124,12 @@ def _validate_dest_currency(dest_currency):
 
 
 def _validate_amount(amount):
+    """
+    Function that checks that the amount to be converted received as input is valid
+
+    :param amount: Amount of currency you want to convert
+    :return:  Validated amount
+    """
     try:
         return np.float(amount)
     except ValueError:
@@ -101,6 +137,12 @@ def _validate_amount(amount):
 
 
 def _validate_reference_date(reference_date):
+    """
+    Function that checks that the reference date entered by the user is valid
+
+    :param reference_date: reference date received as input
+    :return: Validated reference date
+    """
     try:
         if dfcur[reference_date].any():
             return reference_date
@@ -109,13 +151,18 @@ def _validate_reference_date(reference_date):
 
 
 def _convert_currency(amount, src_currency, dest_currency, reference_date):
+    """
+    Function that performs currency conversion from previously validated data.
+
+    :param amount: Validated amount of currency you want to convert
+    :param src_currency: Validated source currency code.
+    :param dest_currency: Validated destination currency code
+    :param reference_date: Validated date on which the operation is executed .
+    :return:
+    """
     currencies_rates = dfcur[reference_date]
-    print(currencies_rates)
     src_value = currencies_rates[src_currency]
-    print(src_value)
     dest_value = currencies_rates[dest_currency]
-    print(dest_value)
-    print(np.divide(np.multiply(np.float(amount), dest_value), src_value))
     return np.divide(np.multiply(np.float(amount), dest_value), src_value)
 
 
--- venv/converter_test.py
+++ venv/converter_test.py
@@ -2,9 +2,53 @@ import converter as c
 import pytest
 
 
-def test_1():
-    assert c._validate_amount(100) == 100
+def test_date_validation():
+    date = list(c.dfcur)[0]
+    assert c._validate_reference_date(date) == date
+
+    with pytest.raises(KeyError):
+        c._validate_reference_date("2020--10") == "2020-02-10"
+    with pytest.raises(KeyError):
+        c._validate_reference_date("2020-2-1") == "2020-02-01"
 
 
-def test_2():
-    assert True == False
+def test_dest_currency_validation():
+    assert c._validate_dest_currency("EUR") == "EUR"
+
+    assert c._validate_dest_currency("eur") == "EUR"
+
+    assert c._validate_dest_currency("Usd") == "USD"
+
+    with pytest.raises(KeyError):
+        c._validate_dest_currency("#54fdc")
+
+    with pytest.raises(AttributeError):
+        c._validate_dest_currency(None)
+
+
+def test_src_currency_validation():
+    assert c._validate_src_currency("EUR") == "EUR"
+
+    assert c._validate_src_currency("eur") == "EUR"
+
+    assert c._validate_src_currency("Usd") == "USD"
+
+    with pytest.raises(KeyError):
+        c._validate_src_currency("#54fdc")
+
+    with pytest.raises(AttributeError):
+        c._validate_src_currency(None)
+
+
+def test_amount_validation():
+    assert c._validate_amount(12) == 12
+    assert c._validate_amount(100.33) == 100.33
+
+    with pytest.raises(ValueError):
+        c._validate_amount("15,1")
+
+    with pytest.raises(ValueError):
+        c._validate_amount("abc")
+
+    with pytest.raises(ValueError):
+        c._validate_amount("15'1")
